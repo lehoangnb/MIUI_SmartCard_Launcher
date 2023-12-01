2fc158450b63d3ac5c0b5f538bbc86301eeb7bc0
lehoangnb <5148339+lehoangnb@users.noreply.github.com>
2023-12-01 14:45:21 +0700

    Work ok!

---
+++ app/src/main/java/com/lehoang/tsmclient/launcher/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lehoang.tsmclient.launcher
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+
+        val btnOpen = findViewById<Button>(R.id.setting)
+        btnOpen.setOnClickListener {
+            val url = "https://tsmclient.miui.com?action=uni_settings&type=0&source_channel=wallet"
+            val tsmclient = Intent(Intent.ACTION_VIEW)
+            tsmclient.setPackage("com.miui.tsmclient")
+            tsmclient.data = Uri.parse(url)
+            startActivity(tsmclient)
+        }
+
+        val btnSetting = findViewById<Button>(R.id.open_tsmclient)
+        btnSetting.setOnClickListener {
+            val url = "tsmclient://swiping"
+            val tsmclient = Intent(Intent.ACTION_VIEW)
+            //tsmclient.setPackage("com.miui.tsmclient")
+            tsmclient.data = Uri.parse(url)
+            startActivity(tsmclient)
+        }
+    }
+}
